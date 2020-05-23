4d866512c9960ff7bdb4239199c49775a43094db
nazirahramli <56263274+nazirahramli@users.noreply.github.com>
2020-05-23 12:25:51 +0800

    Add files via upload

---
+++ infopage.dart
@@ -0,0 +1,16 @@
+class InfoPage {
+  String pid, location, state, description, latitude, longitude, url, contact, address, imagename;
+
+  InfoPage(
+      {this.pid,
+      this.location,
+      this.state,
+      this.description,
+      this.latitude,
+      this.longitude,
+      this.url,
+      this.contact,
+      this.address,
+      this.imagename
+      });
+}
